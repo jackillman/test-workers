342ff55d19fd7d9264c5f079a6f3bcfe395e0fcb
jackill <jackillman@gmail.com>
2018-08-25 13:49:03 +0300

    change regular exp

--- src/app/shared/services/work.service.ts
+++ src/app/shared/services/work.service.ts
@@ -7,10 +7,10 @@ export class WorkService {
   workers
   fromLocalsStorage = []
   constructor() { 
-    let workers = [{id:0,name:"Евгений",birth:"22-12-1986",position:"фронтенд",salary:2000},
-                    {id:1,name:"Андрей",birth:"02-12-1976",position:"бэкэнд",salary:5000},
-                    {id:2,name:"Марина",birth:"20-04-1972",position:"мастер",salary:3000},
-                    {id:3,name:"Полина",birth:"11-02-1966",position:"бэкэнд",salary:2100}
+    let workers = [{id:0,name:"Евгений",birth:"22/12/1986",position:"фронтенд",salary:2000},
+                    {id:1,name:"Андрей",birth:"02/12/1976",position:"бэкэнд",salary:5000},
+                    {id:2,name:"Марина",birth:"20/04/1972",position:"мастер",salary:3000},
+                    {id:3,name:"Полина",birth:"11/02/1966",position:"бэкэнд",salary:2100}
   
     ]
     if(localStorage.getItem("worker-list") !== null){
